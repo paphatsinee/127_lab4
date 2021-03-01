1b8cc3f8248c625d894e4891f8a33042ce29a714
Paphatsinee <paphatsinee.r@ku.th>
2021-03-01 19:05:17 +0700

    Update Lab4_6.c

--- Lab4_6.c
+++ Lab4_6.c
@@ -2,12 +2,24 @@
 int main()
 {
     char st[4][50];
-    int i;
+    char str;
+    char stt;
+    int i,k=0;
     for(i=0;i<4;i++)
     {
-        scanf("%s",st[i]);
+        scanf("%s",st[50]);
     }
+    for(i=0;i<4;i++)
+    {
+        if(k<=strlen(st[i]))
+        {
+            k=strlen(st[i]);
+            putchar(tolower(st));
 
+        }
+
+    }
+    printf("%s",st);
 
 
 
