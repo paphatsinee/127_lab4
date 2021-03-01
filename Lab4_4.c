333d5e27332f3def0e49ac8351135d6d3e037b28
Paphatsinee <paphatsinee.r@ku.th>
2021-03-01 19:26:01 +0700

    Update Lab4_4.c

--- Lab4_4.c
+++ Lab4_4.c
@@ -2,12 +2,26 @@
 void acronym (char *a);
 int main()
 {
-    char b[300];
+    char b[100];
     gets(b);
     acronym(b);
 }
 
 void acronym(char *a)
 {
+    int i,j;
+    char b[100];
+    char *d;
+
+    j=strlen(a);
+    for(i=0;i<j;i++)
+    {
+        if(i==0)
+            printf("%c",toupper(a[0]));
+        else if((a[i]==' ')&&(a[i]!= 'i','of','the','on','at','for','with','a','an','in','and'))
+            printf("%c",toupper(a[i+1]));
+
+    }
+
 
 }
